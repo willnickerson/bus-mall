db0faefb0485514ea6f080689edc87e2f9030669
willnickerson <wnickers@willamette.edu>
2016-08-30 13:17:37 -0700

    graph shows up on page

--- app.js
+++ app.js
@@ -12,6 +12,49 @@ var imageList = document.getElementById('images');
 //this array will be used to keep track of the previous 3 images to avoid repeats
 var oldGroup = [];
 
+var ctx = document.getElementById('my_chart');
+
+var data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+      data: [65, 59, 80, 81, 56, 55, 40],
+    }
+  ]
+};
+
+var myBarChart = new Chart(ctx, {
+  type: 'bar',
+  data: data,
+  // options: options
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
 //creating and loading our item objects into an array
 loadItems();
 
